9a6018a54e7a9019da91d2281bcaab4c64342039
Shubhabrata Ghosh <shubhabrata.g1108@gmail.com>
2025-03-02 11:41:08 +0530

    Cli game ligic implemented

--- hangman/script.js
+++ hangman/script.js
@@ -4,23 +4,52 @@
 
 import words from "./assets/words.json" with { type: "json" };
 
-const getRandomElement = (array) => {
-  const randomValidIdx = Math.floor((Math.random() * 100)) % array.length;
-  return array[randomValidIdx];
-}
-
-function main () {
-  const choiceTable = {
+const getRandomWord = (choice) => {
+  const categories = {
     1: "vegetables",
     2: "fruits",
     3: "birds",
-    4: "animals"
-  }
-  const choice  = parseInt(prompt("choose: 1/2/3/4:"));
+    4: "animals",
+  };
+  const list = words[categories[choice]];
+  const randomValidIdx = Math.floor(Math.random() * 100) % list.length;
+  return list[randomValidIdx];
+};
 
-  const word = getRandomElement(words[choiceTable[choice]])
+const getWordProgress = (lettersArray, remainingLetters) => {
+  return lettersArray
+    .map((letter) => (remainingLetters.has(letter) ? "_" : letter))
+    .join("");
+};
+
+const result = (chances) => {
+  return chances < 1 ? "You Lost!!" : "You won!!";
+};
+
+const game = (word) => {
+  const remainingLetters = new Set(word);
+  let remainingChances = 5;
+
+  while (remainingChances > 0 && remainingLetters.size > 0) {
+    const guessedLetter = prompt("Enter Letter: ");
+    if (remainingLetters.has(guessedLetter)) {
+      remainingLetters.delete(guessedLetter);
+      console.log("Correct Choice");
+    } else {
+      console.log("Wrong Choice");
+      remainingChances--;
+    }
+    console.log(getWordProgress(word.split(""), remainingLetters));
+  }
+  return result(remainingChances);
+};
+
+function main() {
+  const choice = parseInt(prompt("choose: 1/2/3/4:"));
+  const word = getRandomWord(choice);
   console.log(word);
-  
+  const result = game(word);
+  console.log(result);
 }
 
 main();
